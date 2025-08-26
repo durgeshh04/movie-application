3c035064452e83045f1104c2d7b1bf502b641546
durgesh-chaudhari-trj <durgesh.chaudhari@thereturnjourney.com>
2025-08-26 14:30:19 +0530

    movie routing screen template added

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,20 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl text-dark-200 font-bold">
-        Welcome to the Project.
-      </Text>
+      <View className="items-center">
+        <Text className="text-3xl text-dark-200 font-bold">
+          Welcome to the Project.
+        </Text>
+        <Link href="/onboarding" className="text-blue-600 text-lg underline">
+          Onboarding
+        </Link>
+        <Link href="/movie/avenger" className="text-dark-200 text-lg underline">
+          Avenger Movie
+        </Link>
+      </View>
     </View>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const onboarding = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl text-primary font-bold">Onboarding Screen</Text>
+    </View>
+  );
+};
+
+export default onboarding;
+
+const styles = StyleSheet.create({});
